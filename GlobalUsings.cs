a269aa593c9740e193d38e666bf07e75ea60d574
Weslley Luiz <weslleyluiz@outlook.com.br>
2023-04-16 09:11:57 -0300

    Portifolio strutcture creation

---
+++ GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using System.Net.Http.Json;
+global using weslleymurdock.github.io;
+global using weslleymurdock.github.io.Models;
+global using weslleymurdock.github.io.Services;
