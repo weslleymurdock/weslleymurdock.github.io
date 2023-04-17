f3810b9457a35e84ae894316276c20276857de02
Weslley Luiz <weslleyluiz@outlook.com.br>
2023-04-17 16:08:46 -0300

    Resume creation

---
+++ Models/Education.cs
@@ -0,0 +1,12 @@
+﻿namespace weslleymurdock.github.io.Models
+{
+    public class Education
+    {
+        public string SchoolName { get; set; } = string.Empty;
+        public string CourseName { get; set; } = string.Empty;
+        
+        public string Start { get; set; } = string.Empty;
+        public string? End { get; set; }
+
+    }
+}
