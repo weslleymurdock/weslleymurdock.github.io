e7dc0275325aa5e93433f624aba240012f06f7d9
Weslley Murdock <weslleyluiz@outlook.com.br>
2026-03-24 14:24:36 -0300

    chore: remove old .NET 7 site

--- GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-﻿global using Microsoft.AspNetCore.Components.Web;
-global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-global using System.Net.Http.Json;
-global using weslleymurdock.github.io;
-global using weslleymurdock.github.io.Models;
-global using weslleymurdock.github.io.Services;
--- Models/AboutMe.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace weslleymurdock.github.io.Models;
-public class AboutMe
-{
-    public string Description { get; set; } = string.Empty;
-    public List<string> Skills { get; set; } = new();
-    public string DetailOrQuote { get; set; } = string.Empty;
-}
